c7965ca6ccada340c8e764fba8fcf8dd2e8a1e57
druvkotwani <druvkotwani12@gmail.com>
2024-03-09 11:11:11 +0530

    Selected the icon when clicked on it

--- src/components/FAB/Fab.js
+++ src/components/FAB/Fab.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ContactCard from '../Cards/ContactCard';
 
 const feedbackOptions = [
     {
@@ -59,51 +60,7 @@ const OptionDiv = ({ handleToggle }) => {
         <div className='flex flex-col items-end'>
             {
                 clicked && (
-                    <div className='bg-white h-[540px] w-[430px] gap-[32px] rounded-lg flex flex-col items-start '>
-                        <header className='text-[#4D4D4D]  text-xl font-medium border-b-2  px-4 py-4 font-poppins flex items-center justify-center w-full'>
-                            <p className='px-4'>
-                                Let us know about the Issue
-                                <br />
-                                you are facing right now!
-                            </p>
-                        </header>
-
-                        <div className='w-full'>
-                            <div className='flex items-start flex-col justify-start px-4 '>
-                                <p className='text-lg font-poppins font-medium text-[#4D4D4D]'>Choose a section</p>
-                                <select value={selectedOption} onChange={handleOptionChange} className='bg-[#E0E0E0] w-full py-3 px-4 rounded-lg text-lg font-poppins font-medium border-[#CCCCCC] border  '>
-                                    <option value="">Concept Cards</option>
-                                    <option value="option1">Interview Questions</option>
-                                    <option value="option2">Practise Questions</option>
-                                    <option value="option3">Quizzes</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div className='w-full flex flex-col'>
-                            <div className='flex items-start flex-col justify-start px-4 '>
-                                <p className='text-lg font-poppins font-medium text-[#4D4D4D] flex  items-center justify-center gap-1'>
-                                    Describe the issue in detail
-                                    <span>{astrix()}</span>
-                                </p>
-                            </div>
-
-                            <div className='px-4 flex justify-start items-end '>
-                                <textarea className='resize-none w-full h-[200px] bg-[#E0E0E0] rounded-lg px-4 py-3 font-poppins font-medium text-lg border-[#CCCCCC] border' placeholder='Write here...' />
-                                <button className='absolute flex justify-center items-center  bg-[#C7C7C7] px-[10px] py-1 ml-2 mb-2 rounded-md font-medium text-black font-poppins text-[19px]'>
-                                    <span>
-                                        {attach()}
-                                    </span>
-                                    Attach
-                                </button>
-
-                            </div>
-
-
-                            <button className='bg-[#0F0F0F] ml-auto mx-4 px-5 py-[10px] opacity-[60%] rounded-lg text-lg font-poppins font-medium text-[#F8F8F8]  border mt-4'>Submit</button>
-                        </div>
-
-                    </div>
+                    <ContactCard />
                 )
             }
             <div className={` bg-transparent   flex items-end ${!clicked ? 'flex-col gap-6 w-[256px] ' : 'flex-row gap-2'}  `}>
@@ -133,19 +90,22 @@ const OptionDiv = ({ handleToggle }) => {
 const EachOptioin = ({ text, icon, IconName, clicked, setClicked }) => {
 
     const [selectedIcon, setSelectedIcon] = useState('')
+    const [layoutOpen, setLayoutOpen] = useState(false)
 
     const handleClicked = (e) => {
-
         setClicked(!clicked)
     }
 
     return (
-        <div onClick={() => setSelectedIcon(IconName)} key={text} className={`flex justify-center items-center font-poppins rounded-md`}>
+        <div onClick={() => {
+            setSelectedIcon(IconName)
+            handleClicked()
+        }} key={text} className={`flex justify-center items-center font-poppins rounded-md`}>
             {
                 !clicked && <span onClick={handleClicked} className='text-black text-[18px] font-medium px-4 py-2 bg-white rounded-md cursor-pointer'>{text}</span>
             }
 
-            <span onClick={handleClicked} className={`${selectedIcon === IconName ? 'bg-red-200' : ''}     cursor-pointer `}>
+            <span onClick={(e) => selectedIcon === IconName ? setClicked(!clicked) : ''} className={`${selectedIcon ? 'bg-red-200 rounded-full ' : ''}     cursor-pointer `}>
                 {React.cloneElement(icon, { setSelectedIcon })} {/* Passing setSelectedIcon to the icon */}
             </span>
         </div>
@@ -328,11 +288,3 @@ function ContactUS({ setSelectedIcon }) {
     )
 }
 
-function astrix() {
-    return (
-        <svg width="18" height="18" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8.00013 4V12M11.4641 6L4.53613 10M4.53613 6L11.4641 10" stroke="#FD443A" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-
-    )
-}
